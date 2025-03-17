8f2933ea9d76f7a78bf96c6fb5119fdcda8fcf0d
Kellly <kellly.kl.yang@myviewboard.com>
2025-03-17 16:33:24 +0800

    feat: add Fastify support to NestJS app
    - Add "@fastify/static" dependency to package.json
    - Add "@nestjs/platform-fastify" dependency to package.json
    - Change NestFactory.create to NestFactory.create<NestFastifyApplication> in main.ts
    - Add new FastifyAdapter() parameter to NestFactory.create in main.ts
    - Change app.listen to include "0.0.0.0" as a parameter in main.ts

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,16 @@
 import { NestFactory } from '@nestjs/core';
+import {
+  FastifyAdapter,
+  NestFastifyApplication,
+} from '@nestjs/platform-fastify';
 import { AppModule } from './app.module';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create<NestFastifyApplication>(
+    AppModule,
+    new FastifyAdapter(),
+  );
 
   const config = new DocumentBuilder()
     .setTitle('Todo API')
@@ -15,6 +22,6 @@ async function bootstrap() {
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('api', app, document);
 
-  await app.listen(process.env.PORT ?? 3000);
+  await app.listen(process.env.PORT ?? 3000, '0.0.0.0');
 }
 bootstrap();
